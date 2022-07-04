a4c07fd7132fc200a6ab06465cf979ef1edeb2e0
andersc <andersc@vergic.com>
2022-07-04 09:00:32 +0200

    fix: PaoTable.js fixes, now uses myPao local file

--- src/components/PaoTable.js
+++ src/components/PaoTable.js
@@ -6,36 +6,42 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { paoData } from '../paoData';
+import { paoData } from '../myPao';
 
 export const PaoTable = () => {
+	const names = paoData.filter(({ person }) => person).length;
+	const completeEntries = paoData.filter(({ person, action, object }) => person && action && object).length;
 	return (
-		<TableContainer component={Paper}>
-			<Table sx={{ minWidth: 650 }} aria-label="simple table">
-				<TableHead>
-					<TableRow>
-						{Object.keys(paoData[0])
-							.map(key => <TableCell key={key}>{key}</TableCell>)
-						}
-					</TableRow>
-				</TableHead>
-				<TableBody>
-					{paoData.map(({ string, person, action, object }) => (
-						<TableRow
-							key={string}
-							sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-						>
-							<TableCell component="th" scope="row">
-								{string}
-							</TableCell>
-							<TableCell align="right">{person}</TableCell>
-							<TableCell align="right">{action}</TableCell>
-							<TableCell align="right">{object}</TableCell>
+		<>
+			<h3>names:{names}</h3>
+			<h3>complete:{completeEntries}</h3>
+			<TableContainer component={Paper}>
+				<Table sx={{ width: 400 }} aria-label="simple table">
+					<TableHead>
+						<TableRow>
+							{Object.keys(paoData[0])
+								.map(key => <TableCell key={key}>{key}</TableCell>)
+							}
 						</TableRow>
-					))}
-				</TableBody>
-			</Table>
-		</TableContainer>
+					</TableHead>
+					<TableBody>
+						{paoData.map(({ string, person, action, object }) => (
+							<TableRow
+								key={string}
+								sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+							>
+								<TableCell component="th" scope="row">
+									{string}
+								</TableCell>
+								<TableCell align="left">{person}</TableCell>
+								<TableCell align="left">{action}</TableCell>
+								<TableCell align="left">{object}</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
+			</TableContainer>
+		</>
 	);
 }
 
